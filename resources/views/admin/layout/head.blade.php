214907b05024592e8eb6fcadf211144f51b0a7bc
tiennxph11054 <86552006+tiennxph11054@users.noreply.github.com>
2021-10-05 23:01:16 +0700

    first commit

---
+++ resources/views/admin/layout/head.blade.php
@@ -0,0 +1,22 @@
+<!-- Mirrored from designreset.com/cork/ltr/demo3/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Sep 2021 13:58:26 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('backend/assets/img/favicon.ico')}}" />
+    <link href="{{asset('backend/assets/css/loader.css')}}" rel="stylesheet" type="text/css" />
+    <script src="{{asset('backend/assets/js/loader.js')}}"></script>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="{{asset('backend/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('backend/assets/css/plugins.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    <link href="{{asset('backend/plugins/apex/apexcharts.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('backend/assets/css/dashboard/dash_1.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
+</head>
